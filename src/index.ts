501decdcd08d59d2028edd0b9c00ef8c37c1e883
Mohamed780050 <mohamed39omer@gmail.com>
2025-02-17 17:49:28 +0200

    check if the user is found or not

--- src/index.ts
+++ src/index.ts
@@ -11,38 +11,3 @@ if (!username) {
   process.exit(1);
 }
 await fetchUserData(username)
-// const response = await Axios.get(`/users/${username}/events`);
-// const myObject = {
-//   PushEvent: 0,
-//   CreateEvent: 0,
-//   DeleteEvent: 0,
-//   ForkEvent: 0,
-//   PublicEvent: 0,
-//   WatchEvent: 0,
-//   PullRequestEvent: 0,
-//   PullRequestReviewEvent: 0,
-//   PullRequestReviewCommentEvent: 0,
-//   IssuesEvent: 0,
-//   IssueCommentEvent: 0,
-//   MemberEvent: 0,
-//   TeamAddEvent: 0,
-//   OrgBlockEvent: 0,
-//   WorkflowRunEvent: 0,
-//   CheckRunEvent: 0,
-//   CheckSuiteEvent: 0,
-//   GollumEvent: 0,
-//   SecurityAdvisoryEvent: 0,
-//   DependabotAlertEvent: 0,
-//   RepositoryVulnerabilityAlertEvent: 0,
-//   MarketplacePurchaseEvent: 0,
-//   SponsorshipEvent: 0,
-// };
-// const data: dataInterface[] = response.data;
-// data.forEach((element) => {
-//   myObject[element.type] = myObject[element.type] + 1;
-// });
-// const foundEvent = events.filter((item: types) => myObject[item]);
-// const table = new Table({
-//   head: [...foundEvent],
-// });
-// console.log(table.toString());
--- src/lib/fetchUserData.ts
+++ src/lib/fetchUserData.ts
@@ -1,10 +1,19 @@
-import { Axios } from "./axios.js"
+import { Axios } from "./axios.js";
+import { AxiosError } from "axios";
 import { processEvent } from "./processEvent.js";
-export async function fetchUserData(username:string) {
+import chalk from "chalk";
+export async function fetchUserData(username: string) {
   try {
     const response = await Axios.get(`/users/${username}/events`);
-    processEvent(response.data)
-  } catch (err) {
-    console.log(err)
+    processEvent(response.data);
+  } catch (err: any) {
+    const error: AxiosError = err;
+    switch (error.status) {
+      case 404:
+        console.log(chalk.red("User not found"));
+        break;
+      default:
+        console.log(chalk.red("There is a problem"));
+    }
   }
 }
